4de295b7dcf0f8c03c4049a22e4128aa897336b1
Dushyant <singhdushyant2989@gmail.com>
2023-08-16 17:32:58 +0530

    Added Icons to the heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import AddToDo from "./components/addtodo";
 import Navbar from "./components/navbar";
 import Todoslist from "./components/todoslist";
+import * as AiIcons from "react-icons/ai";
 import "./App.css";
 
 const App = () => {
   return (
     <main className="main">
-      <h1 className="main-heading">TODO REACT + TYPESCRIPT</h1>
+      <h1 className="main-heading">
+        <AiIcons.AiOutlineCalendar />
+        &nbsp; TODO REACT + TYPESCRIPT &nbsp;
+        <AiIcons.AiOutlineCalendar />
+      </h1>
       <Navbar />
       <AddToDo />
       <Todoslist />
